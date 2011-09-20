ed0fde352030c42d3ca88b6755efd92a3e9229c8
stranne <olof.stranne@gmail.com>
2011-09-20 20:30:18 +0200

    Game board is now drawn on a canvas

--- Play-by-Play/Scripts/Play-by-Play.js
+++ Play-by-Play/Scripts/Play-by-Play.js
@@ -49,6 +49,146 @@ window.PlayByPlay = (function ($) {
 
 
 
+    // draw game board
+    window.onload = function () {
+        // seting up canvas
+        var canvas = document.getElementById("gameBoardCanvas");
+        var context = canvas.getContext("2d");
+
+        // editable values
+        var iceColor = "#FFF";
+        var borderColor = "#000";
+        var redLineColor = "#F00";
+        var blueLineColor = "#00F";
+
+        // don't edit below
+        var boardHeight = document.getElementById('gameBoardBackgroundLayer').offsetHeight;
+        var boardWidth = document.getElementById('gameBoardBackgroundLayer').offsetWidth;
+
+        // correct proportions if neccessary
+        var heightProportions = boardHeight / 8;
+        var widthProportions = boardWidth / 5;
+
+        if (heightProportions > widthProportions) {
+            // set height depending on width
+            boardHeight = boardHeight * (widthProportions / heightProportions);
+        }
+        else if (heightProportions < widthProportions) {
+            // set width depending on height
+            boardWidth = boardWidth * (heightProportions / widthProportions);
+        }
+
+        // set static size (used for testing)
+        //var boardHeight = 80;
+        //var boardWidth = 50;
+
+        document.getElementById('gameBoardBackgroundLayer').style.height = boardHeight + 'px';
+        document.getElementById('gameBoardBackgroundLayer').style.width = boardWidth + 'px';
+
+        canvas.height = boardHeight;
+        canvas.width = boardWidth;
+
+        var margin = boardHeight / 80;
+        var borderWidth = boardHeight / 80;
+        var left = margin + borderWidth;
+        var top = margin + borderWidth;
+        var height = boardHeight - top * 2;
+        var width = boardWidth - left * 2;
+        var lineWidth = height / 160;
+
+
+        // draw ice rink
+        context.beginPath();
+        context.arc(left + width / 5, top + height / 8, width / 5, -Math.PI / 2, Math.PI, true);
+        context.arc(left + width / 5, top + height / 8 * 7, width / 5, Math.PI, Math.PI / 2, true);
+        context.arc(left + width / 5 * 4, top + height / 8 * 7, width / 5, Math.PI / 2, 0, true);
+        context.arc(left + width / 5 * 4, top + height / 8, width / 5, 0, -Math.PI / 2, true);
+        context.closePath();
+        context.fillStyle = iceColor;
+        context.fill();
+        context.lineWidth = borderWidth;
+        context.strokeStyle = borderColor;
+        context.stroke();
+
+
+        // draw lines
+        var lineLeft = left + borderWidth / 2;
+        var lineRight = lineLeft + width - borderWidth;
+        context.beginPath();
+        context.strokeStyle = redLineColor;
+
+        // red main middle line
+        context.moveTo(lineLeft, top + height / 2);
+        context.lineTo(lineRight, top + height / 2);
+        context.lineWidth = lineWidth;
+        context.stroke();
+
+        // red goal lines
+        context.moveTo(lineLeft, top + height / 8);
+        context.lineTo(lineRight, top + height / 8);
+        context.moveTo(lineLeft, top + height / 8 * 7 - lineWidth / 4);
+        context.lineTo(lineRight, top + height / 8 * 7 - lineWidth / 4);
+        context.lineWidth = lineWidth / 2;
+        context.stroke();
+
+        // red line between the goals
+        context.moveTo(left + width / 2, top + height / 8);
+        context.lineTo(left + width / 2, top + height / 8 * 7);
+        context.lineWidth = lineWidth / 4;
+        context.stroke();
+
+        // blue lines
+        context.beginPath();
+        context.strokeStyle = blueLineColor;
+        context.moveTo(lineLeft, top + height * 0.3125);
+        context.lineTo(lineRight, top + height * 0.3125);
+        context.moveTo(lineLeft, top + height * 0.6875);
+        context.lineTo(lineRight, top + height * 0.6875);
+        context.lineWidth = lineWidth;
+        context.stroke();
+
+
+        // set up boxes
+        var boxLeft = lineLeft + 'px';
+        var boxRight = lineLeft + 'px';
+        var boxHeight = height * 0.1875 + 'px';
+        var boxWidth = (lineRight - lineLeft) / 2 + 'px';
+        document.getElementById('gameBoardGoalkeeperOpponent').style.marginTop = top + borderWidth + 'px';
+
+        document.getElementById('gameBoardLW').style.left = boxLeft;
+        document.getElementById('gameBoardLW').style.width = boxWidth;
+        document.getElementById('gameBoardLW').style.height = boxHeight;
+
+        document.getElementById('gameBoardRW').style.right = boxRight;
+        document.getElementById('gameBoardRW').style.width = boxWidth;
+        document.getElementById('gameBoardRW').style.height = boxHeight;
+
+        document.getElementById('gameBoardLCW').style.left = boxLeft;
+        document.getElementById('gameBoardLCW').style.width = boxWidth;
+        document.getElementById('gameBoardLCW').style.height = boxHeight;
+
+        document.getElementById('gameBoardRCW').style.right = boxRight;
+        document.getElementById('gameBoardRCW').style.width = boxWidth;
+        document.getElementById('gameBoardRCW').style.height = boxHeight;
+
+        document.getElementById('gameBoardLCD').style.left = boxLeft;
+        document.getElementById('gameBoardLCD').style.width = boxWidth;
+        document.getElementById('gameBoardLCD').style.height = boxHeight;
+
+        document.getElementById('gameBoardRCD').style.right = boxRight;
+        document.getElementById('gameBoardRCD').style.width = boxWidth;
+        document.getElementById('gameBoardRCD').style.height = boxHeight;
+
+        document.getElementById('gameBoardLD').style.left = boxLeft;
+        document.getElementById('gameBoardLD').style.width = boxWidth;
+        document.getElementById('gameBoardLD').style.height = boxHeight;
+
+        document.getElementById('gameBoardRD').style.right = boxRight;
+        document.getElementById('gameBoardRD').style.width = boxWidth;
+        document.getElementById('gameBoardRD').style.height = boxHeight;
+    }
+
+
     var chat = {
         init: function () {
             connection = $.connection.chat;
@@ -92,6 +232,7 @@ window.PlayByPlay = (function ($) {
         });
 
         chat.init();
+
     });
 
     return play;
